1f77065461c559e1eb6527193e996089f2cb4dba
leo-tavares <leonardotavaresdev@gmail.com>
2020-10-11 00:25:11 -0300

    chore: create explicit action for models #5
     - [x] listMealItems
        * List all meal Items for this meal
    
     - [x] addMealItem
        * add one item on meal
    
     - [x] addMealItems
        * add a list of item on meal. this action use [batch](https://nozbe.github.io/WatermelonDB/Actions.html?highlight=batch#batch-updates).

--- src/database/models/Meal.ts
+++ src/database/models/Meal.ts
@@ -1,6 +1,22 @@
 import {Model} from '@nozbe/watermelondb';
-import {children, date, field, readonly} from '@nozbe/watermelondb/decorators';
+import {
+  action,
+  children,
+  date,
+  field,
+  readonly,
+} from '@nozbe/watermelondb/decorators';
 import {Associations} from '@nozbe/watermelondb/Model';
+import MealItems from './MealItems';
+
+interface addMealItem {
+  name: string;
+  quantity: number;
+  quantity_unit: string;
+  carbohydrate: number;
+  protein: number;
+  fat: number;
+}
 
 class Meal extends Model {
   static table = 'meals';
@@ -21,6 +37,49 @@ class Meal extends Model {
   @readonly()
   @date('updated_at')
   updated_at!: Date;
+
+  @action async listMealItems() {
+    const meal_itemsCollections = this.collections.get<MealItems>('meal_items');
+    const meal_items = await meal_itemsCollections.query().fetch();
+    return meal_items;
+  }
+
+  @action async addMealItem({
+    name,
+    quantity,
+    quantity_unit,
+    carbohydrate,
+    protein,
+    fat,
+  }: addMealItem) {
+    const meal_item = this.collections.get<MealItems>('meal_items');
+    const meal_items = await meal_item.create((mealItem) => {
+      mealItem.meal.set(this);
+      mealItem.name = name;
+      mealItem.quantity = quantity;
+      mealItem.quantity_unit = quantity_unit;
+      mealItem.carbohydrate = carbohydrate;
+      mealItem.protein = protein;
+      mealItem.fat = fat;
+    });
+    return meal_items;
+  }
+
+  @action async addMealItems(mealItems: Array<addMealItem>) {
+    const mealItemCollection = this.collections.get<MealItems>('meal_items');
+    const mealItemsBatch = mealItems.map((mealItem) => {
+      return mealItemCollection.prepareCreate((meal_item) => {
+        meal_item.meal.set(this);
+        meal_item.name = mealItem.name;
+        meal_item.quantity = mealItem.quantity;
+        meal_item.quantity_unit = mealItem.quantity_unit;
+        meal_item.carbohydrate = mealItem.carbohydrate;
+        meal_item.protein = mealItem.protein;
+        meal_item.fat = mealItem.fat;
+      });
+    });
+    await this.batch(...mealItemsBatch);
+  }
 }
 
 export default Meal;
