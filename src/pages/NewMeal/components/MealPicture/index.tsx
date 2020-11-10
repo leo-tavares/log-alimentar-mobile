a332515b7b3c455501208689f254d4c095bd7e8f
leo-tavares <leonardotavaresdev@gmail.com>
2020-11-10 15:08:31 -0300

    chore: use Base64 only in MealPicture #8
    base64 option has been configured
    writeTempFile work only in ios, so the image path was deleted in adnroid
    platform

--- src/pages/NewMeal/components/MealPicture/index.tsx
+++ src/pages/NewMeal/components/MealPicture/index.tsx
@@ -10,10 +10,10 @@ import {
 import ImagePicker, {
   Options as CameraOptions,
 } from 'react-native-image-crop-picker';
-import {PixelRatio} from 'react-native';
+import {PixelRatio, Platform} from 'react-native';
 
 const Picture: React.FC = () => {
-  const [picture, setPicture] = useState('');
+  const [uri, setUri] = useState('');
   const cameraOptions: CameraOptions = {
     width: PixelRatio.getPixelSizeForLayoutSize(120),
     height: PixelRatio.getPixelSizeForLayoutSize(60),
@@ -21,34 +21,39 @@ const Picture: React.FC = () => {
     cropperToolbarTitle:
       'faça alguns ajustes para registrar esse momento \u{1F606}',
     mediaType: 'photo',
+    compressImageQuality: 0.8,
+    includeBase64: true,
+    writeTempFile: false, //only work in ios
   };
 
   const takePicture = useCallback(async () => {
     try {
       const image = await ImagePicker.openCamera(cameraOptions);
-      setPicture(image.path);
+      setUri(`data:${image.mime};base64,${image.data}`);
+      if (Platform.OS === 'android') {
+        await ImagePicker.cleanSingle(image.path);
+      }
     } catch (error) {
-      setPicture('');
+      setUri('');
     }
   }, [cameraOptions]);
 
   const replacePictureCb = useCallback(async () => {
-    await ImagePicker.cleanSingle(picture);
-    setPicture('');
+    setUri('');
     await takePicture();
-  }, [picture, takePicture]);
+  }, [takePicture]);
 
   return (
     <CameraContainer>
-      {!picture && (
+      {!uri && (
         <Camera onPress={takePicture}>
           <TextCamera>Que tal registrar uma foto desse momento?</TextCamera>
         </Camera>
       )}
-      {!!picture && (
+      {!!uri && (
         <>
           <MealPicture
-            source={{uri: picture}}
+            source={{uri}}
             resizeMode={'cover'}
             resizeMethod={'scale'}
           />
